36c54067b99a19ec2f38603e6ce92d53f1277c71
Vlad <vladmalpro37@gmail.com>
2023-06-02 17:59:21 +0300

    slider

--- src/components/SeparateProduct.vue
+++ src/components/SeparateProduct.vue
@@ -1,112 +1,75 @@
 <template>
   <div id="app" class="app-container">
-    <!-- You will need v-for loop fro the transition group to work! -->
-    <transition-group
-      id="list"
-      name="swipe"
-      tag="div"
-      class="transition-container"
+    <!-- <vueper-slides>
+      <vueper-slide
+        v-for="(slide, i) in slides"
+        :key="i"
+        :title="slide.title"
+        :content="slide.content"
+      >
+      </vueper-slide>
+    </vueper-slides> -->
+
+    <vueper-slides
+      :visible-slides="3"
+      :slide-ratio="1 / 4"
+      :dragging-distance="70"
     >
-      <!-- Very important, dont use index, as the key. Use some form of unique info, like the name i use here. -->
-      <!-- <img
-        v-for="(picture, i) in pictures"
-        :key="picture.src"
-        :src="picture.src"
-        alt=""
-        class="image"
-      /> -->
-      <img
-        v-for="(picture) in pictures"
-        :key="picture.src"
-        :src="picture.src"
-        alt=""
-        class="image"
+      <template #arrow-left>
+        <i class="icon icon-arrow-left" />
+      </template>
+
+      <template #arrow-right>
+        <i class="icon icon-arrow-right" />
+      </template>
+
+      <vueper-slide
+        class="slide"
+        v-for="i in slides.length"
+        :key="i"
+        :title="i.toString()"
       />
-    </transition-group>
-    <button class="previous" @click="previous">back</button>
-    <button class="next" @click="next">frwrd</button>
+    </vueper-slides>
   </div>
 </template>
 
 <script>
-import * as Hammer from "hammerjs";
+// import * as Hammer from "hammerjs";
+import { VueperSlides, VueperSlide } from "vueperslides";
+import "vueperslides/dist/vueperslides.css";
 
 export default {
+  components: { VueperSlides, VueperSlide },
   name: "SeparateProduct",
   data() {
     return {
-      pictures: [
+      slides: [
         {
-          src: "https://images.unsplash.com/photo-1562821399-d2116888d16b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80",
-          name: "picture1",
+          title: "Slide #1",
+          content: "Slide 1 content.",
         },
         {
-          src: "https://images.unsplash.com/photo-1549875202-c83686e7dad9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80",
-          name: "picture2",
+          title: "Slide #2",
+          content: "Slide 2 content.",
         },
         {
-          src: "https://images.unsplash.com/photo-1489423561257-34e31d8836b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1636&q=80",
-          name: "picture3",
+          title: "Slide #3",
+          content: "Slide 2 content.",
         },
         {
-          src: "https://images.unsplash.com/photo-1567249601327-aee46508fbea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80",
-          name: "picture4",
+          title: "Slide #4",
+          content: "Slide 2 content.",
         },
         {
-          src: "https://images.unsplash.com/photo-1532360007308-dae5348c77c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80",
-          name: "picture",
+          title: "Slide #5",
+          content: "Slide 2 content.",
         },
         {
-          src: "https://images.unsplash.com/photo-1489423561257-34e31d8836b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1636&q=80",
-          name: "picture5",
-        },
-        {
-          src: "https://images.unsplash.com/photo-1567249601327-aee46508fbea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80",
-          name: "picture6",
-        },
-        {
-          src: "https://images.unsplash.com/photo-1532360007308-dae5348c77c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80",
-          name: "picture7",
-        },
-        {
-          src: "https://images.unsplash.com/photo-1489423561257-34e31d8836b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1636&q=80",
-          name: "picture8",
+          title: "Slide #6",
+          content: "Slide 2 content.",
         },
       ],
     };
-  },
-
-  created() {
-    /* making automatic next-picture after 5 seconds */
-    this.interval = setInterval(() => this.next(), 5000);
-  },
-  mounted() {
-    const myElement = document.getElementById("list");
-
-    /* hammer.js instance, after panend, he triggers the same method, like in the methods. */
-
-    const that = this;
-    const hammertime = new Hammer(myElement);
-
-    hammertime.on("panend", function (ev) {
-      if (ev.additionalEvent === "panright") {
-        const lastSlide = that.pictures.pop();
-        that.pictures = [lastSlide].concat(that.pictures);
-      } else if (ev.additionalEvent === "panleft") {
-        const firstPicture = that.pictures.shift();
-        that.pictures = that.pictures.concat(firstPicture);
-      }
-    });
-  },
-  methods: {
-    previous() {
-      const lastSlide = this.pictures.pop();
-      this.pictures = [lastSlide].concat(this.pictures);
-    },
-    next() {
-      const firstPicture = this.pictures.shift();
-      this.pictures = this.pictures.concat(firstPicture);
-    },
   },
 };
 </script>
@@ -115,64 +78,8 @@ export default {
 @import "../styles/variables.scss";
 @import "../styles/banner.scss";
 
-html,
-body {
-  width: 100%;
-  height: 100%;
-  padding: 0;
-  margin: 0;
-}
-.app-container {
-  width: 100%;
-  height: 100%;
-}
-
-.transition-container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-  overflow: visible;
-}
-
-/*
-  Pointer events none, will prevent the draggind of an image, when you want to swipe.
-*/
-
-.image {
-  height: 500px;
-  z-index: 10;
-  pointer-events: none;
-}
-
-.image:first-child {
-  z-index: 5;
-}
-
-.image:last-child {
-  z-index: 5;
-}
-
-.previous {
-  position: absolute;
-  top: 50%;
-  left: 0;
-  z-index: 15;
-}
-
-.next {
-  position: absolute;
-  right: 0;
-  top: 50%;
-  z-index: 15;
-}
-
-/*
-  You can read about the 'move' property of the transition group here:
-  https://vuejs.org/v2/guide/transitions.html#List-Move-Transitions
-*/
-.swipe-move {
-  transition: all 0.3s;
+.slide {
+  border: 1px solid black;
+  margin: 1vh;
 }
 </style>
